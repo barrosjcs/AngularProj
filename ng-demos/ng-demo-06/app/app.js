4fbc2bb57955064643218124164e551276ecc935
Jefferson <Jefferson@PC-BARROSJCS>
2017-06-15 17:43:10 -0300

    demo 6

---
+++ ng-demos/ng-demo-06/app/app.js
@@ -0,0 +1,45 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', ['ngRoute', 'ngAnimate']);    
+
+    app.config(function ($routeProvider, $locationProvider) {
+        //debugger;
+        console.log('routeProvider');
+        $routeProvider
+            .when('/', {
+                controller: 'HomeCtrl',
+                templateUrl: 'app/areas/home/views/indexInner6.html'
+            })
+            .otherwise({
+                //redirectTo: '404.html'
+                controller: 'HomeCtrl',
+                templateUrl: 'app/areas/home/views/indexInner6.html'
+            })
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    });
+
+    //app.run(['$rootScope', function($rootScope) {
+    //    $rootScope.$on('$routeChangeStart', function(event, next, current) {
+    //        console.log('Mudou');
+    //        console.log(current);
+    //        console.log(next);
+    //    });
+
+    //    $rootScope.$on('routeChangeSuccess', function (event, next, current) {
+    //        console.log('Erro');
+    //        console.log(current);
+    //        console.log(previous);
+    //    });
+
+    //    $rootScope.$on('routeChangeError', function (event, current, previous, rejection) {
+    //        console.log('Erro');
+    //        console.log(current);
+    //        console.log(previous);
+    //    });
+    //}]);
+})();
---
+++ ng-demos/ng-demo-06/app/areas/home/controllers/HomeCtrl.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('HomeCtrl', HomeCtrl);
+
+    HomeCtrl.$inject = ['$scope']; 
+
+    function HomeCtrl($scope) {
+        console.log('Entrou em HomeCtrl')
+        $scope.title = "Clientes Cadastrados";
+    }
+})();
